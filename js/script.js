5a2cd63e006c72190ea4b5100f031c0a4f13120b
FikryAS <azizfikry15@gmail.com>
2021-01-26 10:55:50 +0700

    added ui and animation on the page What we do

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@ function animasiIntroOut() {
           complete: function () {
             callMenu();
             // selector mencari hal yang percis dan men triggernya dengan click pada saat function call menu sudah beres dijalankan
-            $("#menu ul li a[href='what-we-do.html']").trigger("click");
+            $("#menu ul li a[href='whatWeDo']").trigger("click");
           },
         }
       );
@@ -49,11 +49,26 @@ function callMenu() {
   });
 
   // function menambahkan class pada element yang di click
-  $("#menu ul li a").click(function () {
+  $("#menu ul li a").click(function (event) {
+    event.preventDefault(); // menstop default dari tag element yang di click
+
     // menghapus class active yang pada element yang serupa kecuali yang di click nya
     $(this).parent("li").addClass("active").siblings().removeClass("active");
     // $(this).parent("li").siblings().removeClass("active"); penggunaan script yang tidak efisien
+
+    // menampilkan isi konten yang dinamis
+    var hrefString = $(this).attr("href");
+    $("#" + hrefString).show();
+    window[hrefString]();
+    // pendeklarasian ini akan merubah menjadi suatu function
   });
+}
+
+//
+function whatWeDo() {
+  $("#whatWeDo img").velocity("transition.flipYIn", { duration: 1500 });
+  $("#whatWeDo .title").velocity("transition.slideUpIn", { duration: 1500 });
+  $("#whatWeDo div").velocity("transition.slideDownIn", { duration: 1500 });
 }
 
 $(document).ready(function () {
@@ -64,4 +79,5 @@ $(document).ready(function () {
  * stagger satuannya mili sec
  * complete akan dijalankan apabila effect dari velocity parentnya selesai
  * siblings mencari element yang serupa misalkan li dia akan mencari element li lainnya
+ * show() merubah display yang asalnya none menjadi block
  */
